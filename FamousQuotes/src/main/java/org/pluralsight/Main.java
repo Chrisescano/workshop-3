5bb9cc4bed58962e8829b1b2b9a62f734a765cb5
Christian Escano <escanochristian21@gmail.com>
2023-10-17 13:59:31 -0400

    Added: 10 famous quotes

--- FamousQuotes/src/main/java/org/pluralsight/Main.java
+++ FamousQuotes/src/main/java/org/pluralsight/Main.java
@@ -1,9 +1,18 @@
 package org.pluralsight;
 
-
-
 public class Main {
     public static void main(String[] args) {
-
+        String[] famousQuotes = {
+                "Excellence is not being the best. It is doing your best",
+                "Life is a journey, not a destination",
+                "Life isn't about finding yourself. Life is about creating yourself",
+                "Don't look back. You're not going that way.",
+                "Life is 10% what happens to you and 90% how you react to it.",
+                "Ever tried. Ever failed. No matter. Try again. Fail again. Fail better.",
+                "Quality is not an act, it is a habit.",
+                "Good, better, best. Never let it rest. 'Til your good is better and your better is best.",
+                "With the new day comes new strength and new thoughts.",
+                "If you're going through hell, keep going."
+        };
     }
 }
